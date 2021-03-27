76c4fa7bc9d28f3fccc3d6582266410d654cdf9d
sadikovaa <79834063+sadikovaa@users.noreply.github.com>
2021-03-27 15:25:39 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 typedef struct s{
     float coefficient=0;
     int degree=0;
 
 } monomial;
-
-bool comparator(monomial &m1, monomial &m2){
+bool comparator(const monomial &m1, const monomial &m2){
     return m1.degree<m2.degree;
 }
-
 class polynomial{
 private:
     std::vector <monomial> members;
+
 public:
-      void sort(){
+    void sort(){
         std::sort(members.begin(), members.end(), comparator);
+        for (int i=0; i< members.size()-1; i++){
+            if (members[i].degree == members[i+1].degree){
+                members[i].coefficient +=members[i+1].coefficient;
+                for (int j=i+1; j< members.size()-1; j++)
+                    members[j] = members[j+1];
+                members.pop_back();
+            }
+        }
 
     }
     int count() const{
@@ -39,15 +46,14 @@ public:
             members(p.members){
         sort();
     }
-    ~polynomial(){
-            members.clear();
-    }
     polynomial& operator = (const polynomial& p){
         if (&p != this)
             this->members=p.members;
         return *this;
     }
-    bool operator == (const polynomial& p){
+    ~polynomial(){
+    }
+    bool operator == (const polynomial& p) const{
         if (members.size() == p.count()){
             for (int i=0; i<members.size(); i++)
                 if (members[i].degree != p.degree(i) || members[i].coefficient != p.coefficient(i))
@@ -56,10 +62,10 @@ public:
             return false;
         return true;
     }
-    bool operator != (const polynomial& p){
+    bool operator != (const polynomial& p) const{
         return !(*this == p);
     }
-    polynomial& operator += (const polynomial& p){
+    polynomial& operator += (const polynomial& p) {
         int j =0;
         for (int i=0; i < p.count(); i++){
             if (members[j].degree == p.degree(i))
@@ -78,7 +84,7 @@ public:
         sort();
         return *this;
     }
-    polynomial operator + (const polynomial& p){
+    polynomial operator + (const polynomial& p) const{
         polynomial newp(*this);
         newp+=p;
         return newp;
@@ -102,7 +108,7 @@ public:
         sort();
         return *this;
     }
-    polynomial operator - (const polynomial& p){
+    polynomial operator - (const polynomial& p) const{
         polynomial newp(*this);
         newp+=p;
         return newp;
@@ -112,9 +118,27 @@ public:
             members[i].coefficient*=a;
         return *this;
     }
-    polynomial operator *(int a){
+    polynomial operator *(int a)  const{
         polynomial newp(*this);
         newp*=a;
+        return newp;
+    }
+    polynomial operator *= (const polynomial& p){
+        std::vector <monomial> newp;
+        for (int i=0; i< this->count(); i++)
+            for (int j=0; j< p.count(); j++){
+                monomial m;
+                m.coefficient=this->coefficient(i)*p.coefficient(j);
+                m.degree=this->degree(i)*p.degree(j);
+                newp.push_back(m);
+            }
+        members = newp;
+        sort();
+        return *this;
+    }
+    polynomial operator *(const polynomial& p)  const{
+        polynomial newp(*this);
+        newp*=p;
         return newp;
     }
     polynomial& operator /=(int a){
@@ -122,7 +146,7 @@ public:
             members[i].coefficient/=a;
         return *this;
     }
-    polynomial operator /(int a){
+    polynomial operator /(int a) const{
         polynomial newp(*this);
         newp/=a;
         return newp;
@@ -132,13 +156,13 @@ public:
 
 };
 std::ostream& operator<<(std::ostream &out, const polynomial& p){
-        for (int i=0; i<p.members.size(); i++){
-            if (p.members[i].coefficient>0)
-                out << "+";
-            out <<p.members[i].coefficient<<"x^"<<p.members[i].degree;
-        }
-        return  out;
+    for (int i=0; i<p.members.size(); i++){
+        if (p.members[i].coefficient>0)
+            out << "+";
+        out <<p.members[i].coefficient<<"x^"<<p.members[i].degree;
     }
+    return  out;
+}
 std:: istream& operator>>(std::istream &in, polynomial& p){
     p.members.clear();
     int n;
@@ -148,6 +172,7 @@ std:: istream& operator>>(std::istream &in, polynomial& p){
     return in;
 }
 int main(){
+
 
     return 0;
 }
