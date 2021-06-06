40ed9d1604b85cc46b45384f710231bd77843799
sadikovaa <79834063+sadikovaa@users.noreply.github.com>
2021-06-06 20:06:39 +0300

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -1,183 +1,247 @@
 #include <iostream>
 
-template<typename T>
-class circular_buffer{
+template<typename T, class Alloc = std::allocator<T>>
+class circular_buffer {
 public:
-    class Iterator{
+    using alloc_type = Alloc;
+    using alloc_traits = std::allocator_traits<Alloc>;
+
+    class Iterator {
     public:
-        T* curr;
-        T* begin;
-        T* end;
-        explicit Iterator():curr(nullptr), begin(nullptr), end(nullptr){}
-        explicit Iterator(T* first, T* begin, T* end) : curr(first), begin(begin), end(end){}
-        Iterator( const Iterator &I){
-            this->curr=I.curr;
-            this->begin=I.begin;
-            this->end=I.end;
+        T *curr;
+        T *begin;
+        T *head;
+        T *end;
+
+        explicit Iterator() : curr(nullptr), begin(nullptr), head(nullptr), end(nullptr) {}
+
+        explicit Iterator(T *first, T *begin, T *end, T *head) : curr(first), begin(begin), head(head), end(end) {}
+
+        Iterator(const Iterator &I) {
+            this->curr = I.curr;
+            this->begin = I.begin;
+            this->end = I.end;
         }
-        Iterator & operator = (const Iterator& I){
-            if (this != I) {
-                this->curr = I.curr;
-                this->begin = I.begin;
-                this->end = I.end;
-            }
+
+        Iterator &operator=(const Iterator &I) {
+            if (this == I)
+                return *this;
+            this->curr = I.curr;
+            this->begin = I.begin;
+            this->end = I.end;
             return *this;
         }
-        Iterator& operator+ (const int n) {
-            for(int i=0; i<n; i++){
+
+        Iterator &operator+=(const int n) {
+            if (n < 0)
+                return *this -= -n;
+            for (int i = 0; i < n; i++) {
                 ++curr;
                 if (curr == end)
                     curr = begin;
             }
             return *this;
         }
-        Iterator& operator- (const int n) {
-            for(int i=0; i<n; i++){
+
+        Iterator operator+(const int n) const {
+            Iterator iter(*this);
+            iter += n;
+            return iter;
+        }
+
+        Iterator &operator-=(const int n) {
+            if (n < 0)
+                return *this += -n;
+            for (int i = 0; i < n; i++) {
                 --curr;
                 if (curr == begin)
                     curr = end;
             }
             return *this;
         }
-        Iterator& operator++ () {
+
+        Iterator operator-(const int n) const {
+            Iterator iter(*this);
+            iter -= n;
+            return iter;
+        }
+
+        Iterator &operator++() {
             ++curr;
             if (curr == end)
                 curr = begin;
             return *this;
         }
-        Iterator& operator-- () {
+
+        Iterator &operator--() {
             if (curr == begin)
                 curr = end;
             --curr;
             return *this;
         }
-        bool operator != (const Iterator& I) const { return curr != I.curr;}
-        bool operator == (const Iterator& I) const { return curr == I.curr;}
-    private:
-        size_t poz(const Iterator& I) const{
-            size_t n=0;
-            T* cur=I.begin;
-            while (cur != curr){
+
+
+    public:
+        size_t poz(const Iterator &I) const {
+            size_t n = 0;
+            T *cur = I.head;
+            while (cur != I.curr) {
+                if (cur == I.end)
+                    cur = I.begin;
                 ++n;
                 ++cur;
-                if (cur == I.end)
-                    curr = I.begin;
             }
             return n;
         }
-    public:
-        bool operator >(const Iterator& I) const {return poz(this) > poz(I);}
-        bool operator <(const Iterator& I) const {return poz(this) < poz(I);}
-        T& operator* () const {return *curr;};
-        T& operator-> () const {return curr;}
-    };
-    
-private:
-    T* _buffer;
-    size_t _n;
-    T* _head;
-    T* _tail;
-    T* _begin;
-    T* _end;
 
-    void plusplus(T*& curr){
+    public:
+        bool operator!=(const Iterator &I) const { return curr != I.curr; }
+
+        bool operator==(const Iterator &I) const { return curr == I.curr; }
+
+        bool operator>(const Iterator &I) const { return poz(*this) > poz(I); }
+
+        bool operator<(const Iterator &I) const { return poz(*this) < poz(I); }
+
+        bool operator<=(const Iterator &I) const { return !(*this > I); }
+
+        bool operator>=(const Iterator &I) const { return !(*this < I); }
+
+        T &operator*() const { return *curr; };
+
+        T &operator->() const { return curr; }
+    };
+
+private:
+    T *_buffer;
+    size_t _n;
+    T *_head;
+    T *_tail;
+    T *_begin;
+    alloc_type _alloc;
+
+    void plusplus(T *&curr) {
         ++curr;
-        if (curr == _end)
+        if (curr == _begin + _n + 1)
             curr = _begin;
     }
-    void minusminus(T*& curr){
+
+    void minusminus(T *&curr) {
         if (curr == _begin)
-            curr = _end;
+            curr = _begin + _n + 1;
         --curr;
     }
+
 public:
-    explicit circular_buffer(size_t n=1): _n(n){
-        _buffer = new T[n];
+    explicit circular_buffer(size_t n = 1, const alloc_type &al = alloc_type()) : _n(n), _alloc(al) {
+        _buffer = alloc_traits::allocate(_alloc, n + 1);
         _head = _buffer;
         _tail = _buffer;
         _begin = _buffer;
-        _end = _buffer+_n;
+
     }
-    circular_buffer(const circular_buffer& b){
-        this->_buffer=b._buffer;
-        this->_n=b._n;
-        this->_head=b._head;
-        this->_tail=b._tail;
-        this->_begin=b._begin;
-        this->_end=b._end;
+
+    ~circular_buffer() {
+        alloc_traits::deallocate(_alloc, _buffer, _n + 1);
     }
-    circular_buffer & operator = (const circular_buffer & b){
-        if (this != b) {
-            this->_buffer = b._buffer;
-            this->_n = b._n;
-            this->_head = b._head;
-            this->_tail = b._tail;
-            this->_begin = b._begin;
-            this->_end = b._end;
-        }
+
+    circular_buffer &operator=(const circular_buffer &b) {
+        if (*this == b)
+            return *this;
+        this->_buffer = b._buffer;
+        this->_n = b._n;
+        this->_head = b._head;
+        this->_tail = b._tail;
+        this->_begin = b._begin;
         return *this;
     }
 
-    T& operator[] (const int&n){
-        if (n>0 && n <_n)
-            return _buffer[n];
-        return _buffer[0];
-    }
-    const T& operator[] (const int&n) const{
-        if (n>0 && n <_n)
-            return _buffer[n];
-        return _buffer[0];
+    T &operator[](const int &n) const {
+        if (_begin + _n + 1 <= _head + n) {
+            return *(_head - (_n + 1 - n));
+        }
+        return *(_head + n);
     }
 
-    Iterator begin() {return Iterator(_head, _begin, _end);}
-    Iterator end() {return Iterator(_tail, _begin, _end);}
-    const Iterator begin() const {return Iterator(_head, _begin, _end);}
-    const Iterator end() const {return Iterator(_tail, _begin, _end);}
-    Iterator& front() const {return &Iterator(_head, _begin, _end);}
-    Iterator& back() const {return &Iterator(_tail, _begin, _end);}
-    
-    void push_front(T val){
+
+    Iterator begin() { return Iterator(_head, _begin, _begin + _n, _head); }
+
+    Iterator end() { return Iterator(_tail, _begin, _begin + _n, _head); }
+
+    T &front() const { return _head; }
+
+    T &back() const {
+        if (_tail == _begin) {
+            if (_head == _begin)
+                return *_tail;
+            return *(_begin + _n);
+        }
+        return *(_tail - 1);
+    }
+
+    void push_front(const T &val) {
         minusminus(_head);
-        *_head=val;
-        if (_tail == _head)
+        alloc_traits::construct(_alloc, _head, val);
+        if (_head == _tail) {
             minusminus(_tail);
+            alloc_traits::destroy(_alloc, _tail);
+        }
     }
 
-    void pop_front(){
+    void pop_front() {
+        if (_head == _tail)
+            return;
+        alloc_traits::destroy(_alloc, _head);
         plusplus(_head);
     }
-    void push_back(const T& val){
-        *_tail=val;
+
+    void push_back(const T &val) {
+        alloc_traits::construct(_alloc, _tail, val);
         plusplus(_tail);
-        if (_tail == _head)
+        if (_tail == _head) {
             plusplus(_head);
+            alloc_traits::destroy(_alloc, _tail);
+        }
     }
-    void pop_back(){
+
+    void pop_back() {
+        if (_head == _tail)
+            return;
         minusminus(_tail);
+        alloc_traits::destroy(_alloc, _tail);
     }
-    void resize(size_t n){
-        T* buffer = new T[n];
-        int i=0;
-        while (_head != _tail && i<n){
-            buffer[i]=*_head;
+
+    void resize(size_t n) {
+        T *buffer = alloc_traits::allocate(_alloc, n + 1);
+        int i = 0;
+        while (_head != _tail && i < n) {
+            alloc_traits::construct(_alloc, *(buffer + i), *_head);
             plusplus(_head);
             ++i;
         }
-        _buffer=buffer;
-        _n=n;
+
+        for (auto j = 0; j < _n + 1; ++j)
+            alloc_traits::destroy(_alloc, (_buffer + j));
+        alloc_traits::deallocate(_alloc, _buffer, _n + 1);
+        _buffer = buffer;
+        _n = n;
         _head = _buffer;
-        _tail = _buffer+i;
-    }
-    void out(){
-        for(auto i=0; i<_n; i++){
-            std::cout << _buffer[i]<< "\n";
-        }
-        std::cout<<"Value of first element: "<<*_head<<"\n";
-        minusminus(_tail);
-        std::cout<<"Value of last element: "<<*(_tail)<<"\n";
+        _tail = _buffer + i;
     }
 
+    size_t size() const {
+        return _n;
+    }
+    friend std::ostream operator<<(std::ostream &out, circular_buffer& b);
 };
+template<typename T>
+std::ostream& operator<<(std::ostream &out, circular_buffer<T>& b){
+    for (auto iter = b._head; iter != b._tail; plusplus(iter))
+        out << *iter << "\n";
+    out << "Value of first element: " << *b._head << "\n";
+    out << "Value of last element: " << b.back() << "\n";
+}
+
 
 int main() {
     return 0;
